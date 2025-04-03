865e503a579a1c52b72d23bbf05121a15c2aa2b9
Jesh Khor <jeshaiahkhor@gmail.com>
2025-04-03 08:10:29 +0800

    updated modify function

--- file_scan/modify_scan.py
+++ file_scan/modify_scan.py
@@ -1,15 +1,20 @@
 # Importing modules
 import os 
+import csv
 import glob
 
 # Setting variables
 directory_location = "/home/pi/data_files/*"
+logfile = "scan_log.csv"
 
 # Checking logfile for most recent update
+with open(logfile, "r") as f:
+    last_line = f.readlines()[-1]
+    print(last_line)
 
 # Printing file results from directory location
 for file in glob.glob(directory_location):
     mod_time = os.path.getmtime(file)
     print(mod_time)
 
-# Updating logfile with check details
+# Updating logfile with one new row of latest check details
