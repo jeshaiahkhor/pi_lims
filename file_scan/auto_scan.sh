74b5e1e77bd74e2d2c45d81da65457082483aedd
Jesh Khor <jeshaiahkhor@gmail.com>
2025-04-16 10:11:30 +0800

    ls

--- file_scan/auto_scan.sh
+++ file_scan/auto_scan.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Automated script to start the master venv, run the scanner, and close the venv
+# Automated script to start the master venv, run the scanner, close the venv, and delete the oldest line (if over threshold)
+file_name=scan_log.csv
+threshold=2000
 
 # If not already there, move to home directory
 cd /home/pi
@@ -12,3 +14,10 @@ python /home/pi/development/file_scan/modify_scan.py
 
 # Deactivate the venv when done
 deactivate
+
+# Finding current number of lines - if past threshold, clear oldest line
+current_lines=$(grep -c . $file_name)
+
+if (($current_lines >= threshold)); then
+	sed -i '2d' $file_name
+fi
